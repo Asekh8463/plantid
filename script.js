c08447f2a5ee640958dd6ad2ec59eab69b6d57e4
Asekh <79318977+Asekh8463@users.noreply.github.com>
2025-04-25 17:00:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -30,6 +30,38 @@ var platesjson = [
  {
   "id":"1/8",
   "load":["20.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg"]
+ },
+ {
+  "id": "2/1",
+  "load":["9.jpeg","18.jpeg","60.jpeg"]
+ },
+ {
+  "id": "2/2",
+  "load":["1A.jpeg","1B.jpeg","1C.jpeg","1D.jpeg","3A.jpeg","3B.jpeg","3C.jpeg","3D.jpeg","3E.jpeg","3F.jpeg","3G.jpeg","3H.jpeg","3I.jpeg","4A.jpeg","4B.jpeg","5A.jpeg","5B.jpeg","5C.jpeg","5D.jpeg","5E.jpeg","5F.jpeg","5G.jpeg","5H.jpeg","5I.jpeg","5J.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg","31A.jpeg","31B.jpeg","60.jpeg"]
+ },
+ {
+  "id": "2/3",
+  "load":["25A.jpeg","25B.jpeg","26A.jpeg","26B.jpeg","27A.jpeg","27B.jpeg","27C.jpeg","27D.jpeg","29.jpeg","30.jpeg"]
+ },
+ {
+  "id": "2/4",
+  "load":["5A.jpeg","5B.jpeg","5C.jpeg","5D.jpeg","5E.jpeg","5F.jpeg","5G.jpeg","5H.jpeg","5I.jpeg","5J.jpeg","6A.jpeg","7A.jpeg","7C.jpeg","11A.jpeg","12A.jpeg","12B.jpeg","12C.jpeg","13A.jpeg","14A.jpeg","15A.jpeg","17.jpeg","18.jpeg","23A.jpeg","23B.jpeg","23C.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg"]
+ },
+ {
+  "id": "2/5",
+  "load":["18.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg","39A.jpeg","39B.jpeg","39C.jpeg","39D.jpeg","39E.jpeg","39F.jpeg","39G.jpeg","39H.jpeg","39I.jpeg","39J.jpeg","39K.jpeg","39L.jpeg","39M.jpeg","39N.jpeg","39O.jpeg","39P.jpeg","52.jpeg"]
+ },
+ {
+  "id": "2/6",
+  "load":["1A.jpeg","1B.jpeg","1C.jpeg","1D.jpeg","8A.jpeg","8B.jpeg","9A.jpeg","10A.jpeg","10B.jpeg","10C.jpeg","10D.jpeg","10E.jpeg","10F.jpeg","10G.jpeg","10H.jpeg","10I.jpeg","33A.jpeg","33B.jpeg","33C.jpeg","34A.jpeg","34B.jpeg","35A.jpeg","35B.jpeg","36.jpeg","37.jpeg","37.jpeg","38A.jpeg","38B.jpeg","38C.jpeg","38D.jpeg","40.jpeg","43.jpeg"]
+ },
+ {
+  "id": "2/7",
+  "load":["5A.jpeg","5B.jpeg","5C.jpeg","5D.jpeg","5E.jpeg","5F.jpeg","5G.jpeg","5H.jpeg","5I.jpeg","5J.jpeg","16.jpeg","20.jpeg","21A.jpeg","21B.jpeg","22A.jpeg","22B.jpeg","22C.jpeg","23A.jpeg","23B.jpeg","23C.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg","31A.jpeg","31B.jpeg","54.jpeg"]
+ },
+ {
+  "id": "2/8",
+  "load":["22A.jpeg","22B.jpeg","22C.jpeg","24A.jpeg","24B.jpeg","24C.jpeg","24D.jpeg","24E.jpeg","24F.jpeg"]
  }
 ]
 
@@ -52,10 +84,11 @@ function hideresults()
 	document.getElementById("parasiteend").style.display = "none";
 	document.getElementById("qtable").style.display = "none";
 	let i1 = 0;
-	while(i1 < 36)
+	while(i1 < 52)
 	{
 		let platedesc = "p" + i1;
 		document.getElementById(platedesc).style.display = "none";
+		document.getElementById(platedesc).src = "#";
 		++i1;
 	}
 }
@@ -71,6 +104,7 @@ function hideresultsrestart()
 	{
 		let platedesc = "p" + i1;
 		document.getElementById(platedesc).style.display = "none";
+		document.getElementById(platedesc).src = "#";
 		++i1;
 	}
 	document.getElementById("qn").innerHTML = "Is your plant any of the following?";
